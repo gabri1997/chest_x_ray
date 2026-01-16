e677b0df6401bb899ec7ea5158957bb10b0109b4
gabri1997 <gabrielerosati97@gmail.com>
2026-01-16 16:06:50 +0100

    baseline updated

--- baseline_net.py
+++ baseline_net.py
@@ -6,12 +6,17 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from dataloader import  ChestXrayTorchDataset
 from dataloader import chest_xray_dataset_loader
-
+from torchmetrics.classification import (
+    MultilabelAUROC,
+    MultilabelAccuracy,
+    MultilabelPrecision,
+    MultilabelRecall,
+    MultilabelF1Score,
+)
+import wandb
 
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD  = [0.229, 0.224, 0.225]
-
-
 
 class Densenet(nn.Module):
 
@@ -19,6 +24,12 @@ class Densenet(nn.Module):
         super(Densenet, self).__init__()
         self.model = models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1)
         in_features = self.model.classifier.in_features
+        # il Linear è una logistic regression che mappa le feature estratte dalla densenet alle classi di output
+        # Logistic regression è un modello per classificazione che trasforma una combinazione lineare delle feature in una probabilità.
+        # ho un vettore di feature x in ingresso , pesi e bias b, e voglio predire la probabilità di ciascuna classe, genero lo score lineare z = w⋅x + b
+        # poi uso la sigmoide per ottenre una probabilità p = sigmoid(z) = 1 / (1 + exp(−z)) intervallo [0, 1], intepretabile come probabilità della classe positiva dato x
+        # si allena con la loss, in particolare la binary cross entropy tra p e la label vera y (0 o 1), essendo un problema multilabel uso BCEWithLogitsLoss che combina sigmoid + BCE in modo numericamente stabile
+        # e avendo 15 classi ne fccio 15 in parallelo, quindi logistic regression = Linear + BCE, fatte sulle features della densenet. 
         self.model.classifier = nn.Linear(in_features=in_features, out_features=num_classes)
 
     def forward(self, x):
@@ -31,12 +42,27 @@ class myNet():
         self.num_workers = num_workers
         self.device = device
         self.learning_rate = learning_rate
+        # il weight decay è una forma di regolarizzazione L2 che aiuta a prevenire l'overfitting tenendo i pesi bassi 
+        # altrimenti il modello potrebbe adattarsi troppo ai dati di training e non generalizzare bene sui dati nuovi
         self.weight_decay = weight_decay
         self.momentum = momentum
         self.step_size = step_size
         self.gamma = gamma
         self.net_type = net_type
         self.batch_size = batch_size
+
+        # wandb
+        wandb.init(project="Chest X-ray Classification", config={
+            "learning_rate": learning_rate,
+            "weight_decay": weight_decay,
+            "momentum": momentum,
+            "step_size": step_size,
+            "gamma": gamma,
+            "num_epochs": num_epochs,
+            "batch_size": batch_size,
+            "net_type": net_type
+        })
+
         
         transform_train = transforms.Compose([
             transforms.Resize(256),
@@ -67,12 +93,28 @@ class myNet():
         self.val_loader = DataLoader(self.val_dataset,batch_size=self.batch_size,shuffle=False,num_workers=self.num_workers,pin_memory=True)
         self.test_loader = DataLoader(self.test_dataset,batch_size=self.batch_size,shuffle=False,num_workers=self.num_workers,pin_memory=True)
 
+        # qui sto allenando tutti i parametri del modello (densenet + classifier) perchè self.model.parameters() include tutti i parametri della rete, quindi sto facendo fine tuning completo
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=self.weight_decay)
         self.criterion = nn.BCEWithLogitsLoss()
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=self.step_size, gamma=self.gamma)
+   
+       
+        K = self.num_classes  # 15
+        self.auroc = MultilabelAUROC(num_labels=K, average="macro").to(self.device)
+
+        # usando micro averaging, calcolo le metriche globalmente sommando i veri positivi, falsi positivi e falsi negativi attraverso tutte le classi
+        # in questo caso se una classe è molto comune cioè ha molti esempi, influenzerà di più la metrica complessiva rispetto a una classe rara, tirando la metrica verso le performance sulle classi più frequenti
+
+        self.acc_micro = MultilabelAccuracy(num_labels=K, threshold=0.5, average="micro").to(self.device)
+        self.prec_micro = MultilabelPrecision(num_labels=K, threshold=0.5, average="micro").to(self.device)
+        self.rec_micro = MultilabelRecall(num_labels=K, threshold=0.5, average="micro").to(self.device)
+        self.f1_micro = MultilabelF1Score(num_labels=K, threshold=0.5, average="micro").to(self.device)
+        self.f1_macro = MultilabelF1Score(num_labels=K, threshold=0.5, average="macro").to(self.device)
+
 
     def train(self):
         for epoch in range(self.num_epochs):
+            self.model.train()
             current_loss_value = 0.0
             for image, lbl in self.train_loader:
                # non_blocking=True per migliorare le prestazioni con pin_memory=True nel DataLoader, rende la copia in GPU asincrona cosi la CPU non aspetta
@@ -90,16 +132,99 @@ class myNet():
                # con il training, calcolo come cambiare w per ridurre la loss, dove ∇w​loss è il gradiente e mi dice in che direzione modificare i pesi
                self.optimizer.step()
                current_loss_value += loss.item()
-            print(f"Epoch [{epoch+1}/{self.num_epochs}], Loss: {current_loss_value/len(self.train_loader)}")
-
             # aggiorna il learning rate secondo lo scheduler durante l'allenamento
             # aggiorno l'iperparametro learning rate ogni epoch, spesso conviene partire con lr alto per fare i primi progressi rapidi
             # e poi 'rifinire' la soluizione con lr più bassi senza oscillare nel intorno al minimo
             # quindi cambio il learning rate per guidare l'optimizer
-            self.scheduler.step()   
+            self.scheduler.step()  
+            print(f"Epoch [{epoch+1}/{self.num_epochs}], Loss: {current_loss_value/len(self.train_loader)}")
+
+            print("Validating...")
+            metrics = self.evaluate()
+
+    @torch.no_grad()
+    def evaluate(self):
+
+        self.auroc.reset()
+        self.acc_micro.reset()
+        self.prec_micro.reset()
+        self.rec_micro.reset()
+        self.f1_micro.reset()
+        self.f1_macro.reset()
+
+        self.model.eval()
 
 
+        metrics = {}
+        tot_loss = 0
+        tot_samples = 0
+        threshold = 0.5
 
+        # non aggiorno i gradienti durante la validazione per risparmiare memoria e calcolo
+        with torch.no_grad():
+
+            # per ciascun batch nel validation set
+            for image, lbl in self.val_loader:
+
+                # sposto i tensori in GPU
+                image = image.to(self.device, non_blocking=True)
+                lbl = lbl.to(self.device, non_blocking=True)
+
+                n_samples = image.size(0)
+                tot_samples += n_samples
+
+                # calcolo il mio output
+                outputs = self.model(image)
+            
+                loss = self.criterion(outputs, lbl.float())
+                tot_loss += loss.item()*n_samples
+
+                # converto gli output in probabilità con la sigmoid, essendo problema multilabel un sample può avere più classi -> non uso softmax che forza la somma delle probabilità a 1            
+                probs = torch.sigmoid(outputs)                        
+
+                # AUROC: usa probs (non binarie)
+                self.auroc.update(probs, lbl.int())
+
+                # Accuracy/Precision/Recall/F1: torchmetrics binarizza con threshold interno
+                self.acc_micro.update(probs, lbl.int())
+                self.prec_micro.update(probs, lbl.int())
+                self.rec_micro.update(probs, lbl.int())
+                self.f1_micro.update(probs, lbl.int())
+                self.f1_macro.update(probs, lbl.int())
+
+            avg_val_loss = tot_loss / max(tot_samples, 1)
+
+            metrics = {
+                "val_loss": avg_val_loss,
+                "auroc_macro": self.auroc.compute().item(),
+                "accuracy_micro": self.acc_micro.compute().item(),
+                "precision_micro": self.prec_micro.compute().item(),
+                "recall_micro": self.rec_micro.compute().item(),
+                "f1_micro": self.f1_micro.compute().item(),
+                "f1_macro": self.f1_macro.compute().item(),
+                "threshold": threshold,
+            }
+
+            print(
+                f"Val loss: {metrics['val_loss']:.4f} | "
+                f"AUROC(macro): {metrics['auroc_macro']:.4f} | "
+                f"F1(micro): {metrics['f1_micro']:.4f} | "
+                f"F1(macro): {metrics['f1_macro']:.4f}"
+            )
+
+            wandb.log({
+                "Validation/Loss": metrics["val_loss"],
+                "Validation/AUROC_macro": metrics["auroc_macro"],
+                "Validation/Accuracy_micro": metrics["accuracy_micro"],
+                "Validation/Precision_micro": metrics["precision_micro"],
+                "Validation/Recall_micro": metrics["recall_micro"],
+                "Validation/F1_micro": metrics["f1_micro"],
+                "Validation/F1_macro": metrics["f1_macro"],
+                "Validation/Threshold": threshold,
+            })
+
+            return metrics
+            
 
 if __name__ == '__main__':
 
@@ -128,6 +253,14 @@ if __name__ == '__main__':
     # ----> loss.backward(), è la backpropagation,git calcola tutti questi gradienti, cioè tutte le derivate parziali della loss rispetto ai pesi e li salva in w.grad per ogni peso w nel modello
     # ----> self.optimizer.step() usa questi gradienti per aggiornare tutti i pesi
     
+
+
+    # Quando calcolo la loss con criterion(logits, y) ottengo un valore che misura l’errore del modello sul batch.
+    # La loss è definita confrontando, per ogni sample e per ogni classe, le predizioni (logits/sigmoid) con le label (multi-hot).
+    # I gradienti rispetto ai pesi non vengono calcolati dalla loss da sola: li ottengo con loss.backward(), che calcola 
+    # ∂loss/∂w per ogni parametro del modello.
+    # Questi gradienti indicano quanto ogni peso influenza la loss e in che direzione va aggiornato per ridurla.
+    # I pesi sono condivisi tra tutti i sample: non esistono “pesi per sample”, ma output per sample e gradienti globali accumulati dal batch.
     
     
     
---
+++ sbatch.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --account=grana_maxillo
+#SBATCH --partition=all_usr_prod
+#SBATCH --job-name=ChestXray_train
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --time=04:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+
+set -euo pipefail
+
+mkdir -p logs
+
+echo "=== JOB INFO ==="
+echo "JobID: ${SLURM_JOB_ID}"
+echo "Node: $(hostname)"
+echo "Start: $(date)"
+echo "CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES:-unset}"
+echo "Working dir: $(pwd)"
+echo
+
+echo "=== GPU ==="
+nvidia-smi || true
+echo
+
+# (opzionale ma utile) evita di riempire la HOME: cache su /work
+export HF_HOME=/work/grana_far2023_fomo/ChestXray/.hf
+export HF_DATASETS_CACHE=/work/grana_far2023_fomo/ChestXray/.hf/datasets
+export HF_HUB_CACHE=/work/grana_far2023_fomo/ChestXray/.hf/hub
+export TORCH_HOME=/work/grana_far2023_fomo/ChestXray/.torch
+export WANDB_DIR=/work/grana_far2023_fomo/ChestXray/wandb
+export PIP_CACHE_DIR=/work/grana_far2023_fomo/ChestXray/.pip-cache
+
+# Attiva venv
+source /work/grana_far2023_fomo/ChestXray/Chest/bin/activate
+
+# (opzionale) thread control: spesso meglio non lasciare default altissimi
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export MKL_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+echo "Python: $(which python)"
+python -V
+echo
+
+# Lancia training
+python baseline_net.py
+
+echo
+echo "End: $(date)"
+echo "=== GPU (end) ==="
+nvidia-smi || true
+echo "=== JOB END ==="
